d16f056f6113ec36f23cd1946deb0e4cc6f77df0
Maurus <mauro08.fiore@gmail.com>
2024-06-24 19:46:57 -0300

    Avanzando tu sabeeeh mi loco
    cambie un toque la ambientacion del mapa, agregue lo ecencial los botones detectan cuando la respuesta es correcta o no pero falta la funcionalidad que pedia que la consigna tuvieran

--- Assets/Scripts/Game_Manager.cs
+++ Assets/Scripts/Game_Manager.cs
@@ -1,18 +1,86 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Game_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Declaramos la lista de Pre-Fabs
+    public GameObject[] PrefabList;
+
+    //Creamos dos variables publicas de tipo "Transform", para tener la ubicacion de Pedestal 1 y 2
+    public Transform Pedestal_1;
+    public Transform Pedestal_2;
+
+    //Declaramos una variable que manejara el dinero
+    public int Cash;
+    public Text CashText;
+
+    //Declaramos 2 variables de tipo "Text", para vincularlo con nuestros textos de cuanto cuesta el articulo
+    public Text Texto_Objeto1;
+    public Text Texto_Objeto2;
+
+    //Crea las variables de valor de Objeto, para simplificar codigo mas adelante
+    public int Objeto1_Value;
+    public int Objeto2_Value;
+
+    public int Objeto1y2_Value;
     void Start()
-    {
-        
+    {     
+        //Calcula un numero Random entre 0 y 49, que sera el cash, y lo coloca en el texto "CashText"
+        Cash = Random.Range(0, 50);
+        CashText.text = Cash + " $";
+
+        //Declaramos dos variables de tipo "int", que son nuestros dos numeros random que usaremos para seleccionar el objeto que saldra
+        int Random_Number_1 = Random.Range(0, PrefabList.Length);
+        int Random_Number_2 = Random.Range(0, PrefabList.Length);
+
+        //Instantea los dos objetos encima de los pedestales correspondientes
+        Instantiate(PrefabList[Random_Number_1], new Vector3 (Pedestal_1.position.x, Pedestal_1.position.y + 1.3f, Pedestal_1.position.z), PrefabList[Random_Number_1].transform.rotation);
+        Instantiate(PrefabList[Random_Number_2], new Vector3 (Pedestal_2.position.x, Pedestal_2.position.y + 1.3f, Pedestal_2.position.z), PrefabList[Random_Number_2].transform.rotation);
+
+        //Asigna valor a las variables de valor de Objeto
+        Objeto1_Value = PrefabList[Random_Number_1].GetComponent<Value_Script>().Value;
+        Objeto2_Value = PrefabList[Random_Number_2].GetComponent<Value_Script>().Value;
+
+        Texto_Objeto1.text = Objeto1_Value + "  $";
+        Texto_Objeto2.text = Objeto2_Value + "  $";
+
+        Objeto1y2_Value = Objeto1_Value + Objeto2_Value;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void No_Alcanza_Button() 
     {
-        
+        if (Cash < Objeto1y2_Value)
+        {
+            Debug.Log("Correct");
+        }
+        else 
+        {
+            Debug.Log("InCorrect");
+        }
+    }
+    public void Alcanza_Justo_Button()
+    {
+        if (Cash == Objeto1y2_Value)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("InCorrect");
+        }
+    }
+
+    public void Alcanza_y_Sobra_Button()
+    {
+        if (Cash > Objeto1y2_Value)
+        {
+            Debug.Log("Correct");
+        }
+        else
+        {
+            Debug.Log("InCorrect");
+        }
     }
 }
