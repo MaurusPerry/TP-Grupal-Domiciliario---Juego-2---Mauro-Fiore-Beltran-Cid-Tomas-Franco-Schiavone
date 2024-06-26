5f265198bc47063ef761a239317fd908626d91d8
tomasschia <148562630+tomasschia@users.noreply.github.com>
2024-06-26 08:35:20 -0300

    comentado mis scripts
    en este commit se omenta el scrip que concierne a los objetos y el comportamiento de estos, con su valor, y rotacion respectiva a la hora de la muestra

--- Assets/Scripts/Value_Script.cs
+++ Assets/Scripts/Value_Script.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Value_Script : MonoBehaviour
 {
+   //Script que lo contienen todos los objetos con precios y que seran mostrados encima de los pedestales 
+    
+    
+    
+    //Variable d etipo int que le da el precio a los bjetos que lo contenga
     public int Value;
+    //variable que establece la velocidad con la cual rotan os objetos cuando se muestran 
     private float Rotation_Velocity = 40;
 
     void Start()
@@ -14,6 +20,7 @@ public class Value_Script : MonoBehaviour
 
     void Update()
     {
+        //funcion del transform para que rote 
         transform.Rotate(0, Rotation_Velocity * Time.deltaTime, 0, Space.World);
     }
 }
